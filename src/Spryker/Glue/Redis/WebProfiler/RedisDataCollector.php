ead831f15402f42f7010c06b11bdd8688218619a
Vitalii Ivanov <vitalii.ivanov@spryker.com>
2026-01-05 12:24:28 +0200

    FRW-11268: Add Profiler integration for GlueApplication (#150)
    FRW-11268 Add WebProfiler for Glue GlueBackend GlueStorefront applications

---
+++ src/Spryker/Glue/Redis/WebProfiler/RedisDataCollector.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\Redis\WebProfiler;
+
+use Spryker\Shared\Redis\Logger\RedisLoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Throwable;
+
+class RedisDataCollector extends DataCollector
+{
+    /**
+     * @var string
+     */
+    protected const COLLECTOR_NAME = 'redis';
+
+    /**
+     * @var \Spryker\Shared\Redis\Logger\RedisLoggerInterface
+     */
+    protected $redisLogger;
+
+    /**
+     * @param \Spryker\Shared\Redis\Logger\RedisLoggerInterface $redisLogger
+     */
+    public function __construct(RedisLoggerInterface $redisLogger)
+    {
+        $this->redisLogger = $redisLogger;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @param \Throwable|null $exception
+     *
+     * @return void
+     */
+    public function collect(Request $request, Response $response, ?Throwable $exception = null): void
+    {
+        $this->data['calls'] = $this->redisLogger->getLogs();
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return static::COLLECTOR_NAME;
+    }
+
+    /**
+     * @return void
+     */
+    public function reset(): void
+    {
+        $this->data = [];
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getCalls(): array
+    {
+        return $this->data['calls'];
+    }
+}
